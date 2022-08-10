7c04dc75a056e63551b51449145b6ec75b1ad155
DELL <tawhidul.alamin@gmail.com>
2022-08-10 22:07:08 +0600

    final

---
+++ app/src/main/java/com/abc/adfff/MainActivity.java
@@ -0,0 +1,14 @@
+package com.abc.adfff;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
